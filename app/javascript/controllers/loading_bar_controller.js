6483a7c4fa5f22132a614e525cd849a37c7b2fd2
Andy Leverenz <andy@justalever.com>
2024-08-13 13:03:49 -0500

    Init

---
+++ app/javascript/controllers/loading_bar_controller.js
@@ -0,0 +1,48 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["bar", "percentage"]
+
+  connect() {
+    this.hideBar()
+    this.setupTurboListeners()
+  }
+
+  setupTurboListeners() {
+    document.addEventListener("turbo:before-visit", this.showBar.bind(this))
+    document.addEventListener("turbo:visit", this.startProgress.bind(this))
+    document.addEventListener("turbo:load", this.hideBar.bind(this))
+  }
+
+  showBar() {
+    this.element.classList.remove("hidden")
+    this.progress = 0 // Initialize progress here
+    this.barTarget.style.width = "0%"
+    this.percentageTarget.textContent = "0%" // Initialize percentage text
+    this.interval = setInterval(() => this.updateProgress(), 100) // Start progress update interval
+  }
+
+  startProgress() {
+    this.progress = 0 // Reset progress on visit
+    clearInterval(this.interval) // Clear any existing interval
+    this.interval = setInterval(() => this.updateProgress(), 100) // Start a new interval
+  }
+
+  updateProgress() {
+    this.progress += Math.random() * 10
+    if (this.progress >= 100) {
+      this.progress = 100
+      this.hideBar() // Stop the interval and hide the bar when complete
+      clearInterval(this.interval)
+    }
+    this.barTarget.style.width = `${this.progress}%`
+    this.percentageTarget.textContent = `${Math.round(this.progress)}%`
+  }
+
+  hideBar() {
+    this.barTarget.style.width = "100%"
+    setTimeout(() => {
+      this.element.classList.add("hidden")
+    }, 300)
+  }
+}
